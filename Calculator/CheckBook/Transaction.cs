816fef0424b19dc421e45fc9fad7a7d0950e9b84
sunkarasaikiran <sunkara031991@gmail.com>
2015-04-08 21:00:46 -0400

    assignment

---
+++ Calculator/CheckBook/Transaction.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculator.CheckBook
+{
+    public class Transaction: BaseVM
+    {
+        private CheckBookVM _VM;
+        public CheckBookVM VM
+        {
+            get { return _VM; }
+            set { _VM = value; OnPropertyChanged(); }
+        }
+        
+        private DateTime _Date;
+        public DateTime Date
+        {
+            get { return _Date; }
+            set { _Date = value; OnPropertyChanged(); }
+        }
+
+        private string _Payee;
+        public string Payee
+        {
+            get { return _Payee; }
+            set { _Payee = value; OnPropertyChanged(); }
+        }
+
+        private string _Account;
+        public string Account
+        {
+            get { return _Account; }
+            set { _Account = value; OnPropertyChanged(); if (VM != null) VM.OnPropertyChanged("Accounts"); }
+        }
+
+        private double _Amount;
+        public double Amount
+        {
+            get { return _Amount; }
+            set { _Amount = value; OnPropertyChanged(); }
+        }
+
+        private string _Tag;
+        public string Tag
+        {
+            get { return _Tag; }
+            set { _Tag = value; OnPropertyChanged(); }
+        }
+        
+
+    }
+
+    public class CheckBookVM: BaseVM
+    {
+        private ObservableCollection<Transaction> _Transactions;
+        public ObservableCollection<Transaction> Transactions
+        {
+            get { return _Transactions; }
+            set { _Transactions = value; OnPropertyChanged(); OnPropertyChanged("Accounts"); }
+        }
+
+        public IEnumerable<string> Accounts
+        {
+            get { return Transactions.Select(t=> t.Account).Distinct(); }
+        }
+        
+
+        public void Fill()
+        {
+            Transactions = new ObservableCollection<Transaction>( new[] {
+                new Transaction { VM=this, Date= DateTime.Now.AddDays(-1), Account="Checking", Payee="Moshe", Amount=30, Tag="Food" },
+                new Transaction { VM=this, Date= DateTime.Now.AddDays(-3), Account="Checking", Payee="Tim", Amount=130, Tag="Auto" },
+                new Transaction { VM=this, Date= DateTime.Now.AddDays(-4), Account="Checking", Payee="Moshe", Amount=35, Tag="Food" },
+                new Transaction { VM=this, Date= DateTime.Now.AddDays(-5), Account="Checking", Payee="Bracha", Amount=35, Tag="Food" },
+                new Transaction { VM=this, Date= DateTime.Now.AddDays(-6), Account="Checking", Payee="Tim", Amount=20, Tag="Auto" },
+                new Transaction { VM=this, Date= DateTime.Now.AddDays(-1), Account="Credit", Payee="Moshe", Amount=30, Tag="Food" },
+                new Transaction { VM=this, Date= DateTime.Now.AddDays(-2), Account="Credit", Payee="Bracha", Amount=30.5, Tag="Food" },
+                new Transaction { VM=this, Date= DateTime.Now.AddDays(-3), Account="Credit", Payee="Tim", Amount=130, Tag="Auto" },
+                new Transaction { VM=this, Date= DateTime.Now.AddDays(-4), Account="Credit", Payee="Moshe", Amount=35, Tag="Food" },
+                new Transaction { VM=this, Date= DateTime.Now.AddDays(-5), Account="Credit", Payee="Bracha", Amount=35, Tag="Food" },
+                new Transaction { VM=this, Date= DateTime.Now.AddDays(-2), Account="Checking", Payee="Bracha", Amount=30.5, Tag="Food" },
+                new Transaction { VM=this, Date= DateTime.Now.AddDays(-6), Account="Credit", Payee="Tim", Amount=20, Tag="Auto" },
+            });
+        }
+    }
+}
---
+++ CalculatorTests/CheckBookTest.cs
@@ -0,0 +1,174 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Calculator.CheckBook;
+using System.Linq;
+using System.Collections.ObjectModel;
+
+namespace CalculatorTests
+{
+    [TestClass]
+    public class CheckBookTest
+    {
+        [TestMethod]
+        public void FillsUpProperly()
+        {
+            var ob = new CheckBookVM();
+
+            Assert.IsNull(ob.Transactions);
+
+            ob.Fill();
+
+            Assert.AreEqual(12, ob.Transactions.Count);
+        }
+
+        [TestMethod]
+        public void CountofEqualsMoshe()
+        {
+            var ob = new CheckBookVM();
+            ob.Fill();
+
+            var count = ob.Transactions.Where(t => t.Payee == "Moshe").Count();
+
+            Assert.AreEqual(4, count);
+        }
+
+        [TestMethod]
+        public void SumOfMoneySpentOnFood()
+        {
+            var ob = new CheckBookVM();
+            ob.Fill();
+
+            var category = "Food";
+
+            var food = ob.Transactions.Where(t => t.Tag == category);
+
+            var total = food.Sum(t => t.Amount);
+
+            Assert.AreEqual(261, total);
+
+        }
+
+        [TestMethod]
+        public void Group()
+        {
+            var ob = new CheckBookVM();
+            ob.Fill();
+
+            var total = ob.Transactions.GroupBy(t => t.Tag).Select(g => new { g.Key, Sum = g.Sum(t => t.Amount) });
+
+            Assert.AreEqual(261, total.First().Sum);
+            Assert.AreEqual(300, total.Last().Sum);
+        }
+        [TestMethod]
+        public void averagetransactionamount()
+        {
+            var ob = new CheckBookVM();
+            ob.Fill();
+            var avg = ob.Transactions.GroupBy(t => t.Tag).Select(g => new { g.Key, Avge = g.Average(t => t.Amount) });
+            Assert.AreEqual(32.625, avg.First().Avge);
+            Assert.AreEqual(75, avg.Last().Avge);
+        }
+        [TestMethod]
+        public void amounteachpayee()
+        {
+            var ob = new CheckBookVM();
+            ob.Fill();
+            var tomoche = ob.Transactions.Where(t => t.Payee == "Moshe");
+            var totalmoche = tomoche.Sum(t => t.Amount);
+            Assert.AreEqual(130, totalmoche);
+            var totim = ob.Transactions.Where(t => t.Payee == "Tim");
+            var totaltim = totim.Sum(t => t.Amount);
+            Assert.AreEqual(300, totaltim);
+            var tobracha = ob.Transactions.Where(t => t.Payee == "Bracha");
+            var totalbracha = tobracha.Sum(t => t.Amount);
+            Assert.AreEqual(131, totalbracha);
+
+
+
+
+        }
+        [TestMethod]
+        public void amounteachonfood()
+        {
+            var ob = new CheckBookVM();
+            ob.Fill();
+            var tomoche = ob.Transactions.Where(t => t.Payee == "Moshe");
+            var category = "Food";
+            var food = ob.Transactions.Where(t => t.Payee == "Moshe" & t.Tag == category);
+            var total = food.Sum(t => t.Amount);
+            Assert.AreEqual(130, total);
+            var totim = ob.Transactions.Where(t => t.Payee == "Tim");
+            var category1 = "Food";
+            var food1 = ob.Transactions.Where(t => t.Payee == "Tim" & t.Tag == category1);
+            var total1 = food1.Sum(t => t.Amount);
+            Assert.AreEqual(0, total1);
+            var tobracha = ob.Transactions.Where(t => t.Payee == "Bracha");
+            var category2 = "Food";
+            var food2 = ob.Transactions.Where(t => t.Payee == "Bracha" & t.Tag == category2);
+            var total2 = food2.Sum(t => t.Amount);
+            Assert.AreEqual(131, total2);
+
+
+
+
+        }
+        [TestMethod]
+        public void dates()
+        {
+            var ob = new CheckBookVM();
+            ob.Fill();
+            var myDate = "4-4-2015";
+            var myDate1 = "4-7-2015";
+            var count = ob.Transactions.Where(t => t.Date > Convert.ToDateTime(myDate) & t.Date < Convert.ToDateTime(myDate1)).Count();
+            Assert.AreEqual(6, count);
+        }
+        [TestMethod]
+        public void datesaccountswasused()
+        {
+            var ob = new CheckBookVM();
+            ob.Fill();
+            var trans1 = ob.Transactions.Where(t => t.Account == "Checking");
+            var date1 = trans1.OrderBy(t => t.Date).Count();
+            Assert.AreEqual(6, date1);
+            var trans2 = ob.Transactions.Where(t => t.Account == "Credit");
+            var date2 = trans2.OrderBy(t => t.Date).Count();
+            Assert.AreEqual(6, date2);
+        }
+        [TestMethod]
+
+        public void automoney()
+        {
+            var ob = new CheckBookVM();
+            ob.Fill();
+            var category = "Auto";
+
+            var auto = ob.Transactions.Where(t => t.Tag == category & t.Account == "Checking");
+
+            var total = auto.Sum(t => t.Amount);
+
+            Assert.AreEqual(150, total);
+            var auto1 = ob.Transactions.Where(t => t.Tag == category & t.Account == "Credit");
+
+            var total1 = auto1.Sum(t => t.Amount);
+
+            Assert.AreEqual(150, total1);
+            Assert.IsTrue(total == total1);
+        }
+        [TestMethod]
+
+        public void accountstransaction()
+        {
+            var ob = new CheckBookVM();
+            ob.Fill();
+
+            var date = DateTime.Parse("4/5/2015");
+            var date1 = DateTime.Parse("4/8/2015");
+            var trans = ob.Transactions.Where(t => (t.Date >= date.Date) & (t.Date < date1.Date) & t.Account == "Checking").Count();
+
+            Assert.AreEqual(3, trans);
+            var trans1 = ob.Transactions.Where(t => (t.Date >= date.Date) & (t.Date < date1.Date) & t.Account == "Credit").Count();
+            Assert.AreEqual(3, trans1);
+
+        }
+    }
+}
